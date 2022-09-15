b8284313387d79e33242bce558dde2c3b5b92c34
Matheus Bastos <matheus.mab432@gmail.com>
2022-09-15 18:21:21 -0300

    Finishing react-optimization section

--- react-optimization/src/App.tsx
+++ react-optimization/src/App.tsx
@@ -3,6 +3,7 @@ import {
   CSSProperties,
   useCallback,
   useEffect,
+  useMemo,
   useState,
 } from 'react';
 import './App.scss';
@@ -11,6 +12,7 @@ import { Button } from './components/UI/Button';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   // NOTE since this changes on every render, it will cause Button, a memo() component, to be re-evaluated
   // * since it's props will technically change.
@@ -20,20 +22,27 @@ function App() {
 
   // NOTE to fix this, toggleParagraph should be moved to a useCallback hook
   const toggleParagraph = useCallback(() => {
+    if (!allowToggle) return;
+
+    // NOTE to avoid stale/outdated state, the functional form of setState is used
+    // * should be always used whenever a state change is dependent on a previous state
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, [allowToggle]);
+
+  // NOTE for React.memo() to work for objects or arrays, their must be wrapped in useMemo()
+  const buttonStyles = useMemo<CSSProperties>(() => ({ marginTop: '8px' }), []);
+
+  const allowToggleHandler = useCallback(() => {
+    setAllowToggle(true);
   }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <Button
-        onClick={toggleParagraph}
-        disabled={false}
-        // NOTE it is also possible to set nested properties via the HTMLAttributes property
-        // style={getButtonStyles()}
-      >
-        Click me!
+      <Button onClick={toggleParagraph} disabled={false} style={buttonStyles}>
+        Toggle Paragraph
       </Button>
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       {/* NOTE only the difference between the virtual React DOM snapshot will be re-rendered */}
       {/* // * so whenever showParagraph's state changes this is re-rendered */}
       {/* // * {showParagraph && <p>This is new!</p>} */}
