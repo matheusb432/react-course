0d28e3a9c6f43be8e258cde9b66e57906a3eae58
Matheus Bastos <matheus.mab432@gmail.com>
2022-09-17 18:41:56 -0300

    Continuing forms section and adding general notes

--- 8-forms-and-user-input/src/components/SimpleInput.tsx
+++ 8-forms-and-user-input/src/components/SimpleInput.tsx
@@ -1,9 +1,37 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
+import { useInputRef } from '../hooks';
+
 const SimpleInput = () => {
+  // NOTE refs to manage input state are ideal when the form value is only important on submit
+  const nameInputRef = useInputRef();
+  // NOTE while stateful values are ideal when the value is important on every change
+  // * Like when validating on every keystroke
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnteredName(event.target.value);
+  };
+
+  const submitHandler = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    if (!enteredName.trim()) return;
+    console.log(enteredName);
+
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
